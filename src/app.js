ae742a60684bfc5a5f109271cc867a50c6a4a741
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-01-09 20:40:42 +0300

    Add basic route to test server

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@
 - Application entry point
 -----------------------------------------------------------------*/
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// Default middlewares
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/../public`));
+
+app.route('/').get((req, res) => {
+    res.status(200).json({
+        message: 'Hello Ticketo.',
+    });
+});
 
 // EXPORT APP
 module.exports = app;
